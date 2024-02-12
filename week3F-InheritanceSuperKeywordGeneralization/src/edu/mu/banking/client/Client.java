7666bb48bee63673d877bece24d296392f515429
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-02-12 09:43:12 -0600

    Account listing and account closing features added to Client
    - listOpenAccounts(): prints all the open accounts
    - closeAllAccount(): closes all the open accounts (SavingsAccount and
    CheckingAccount), by removing from the ArrayList.
    - closeAllSavingsAccount(): Only closes the SavingsAccount, by only
    removing the SavingsAccount from the Account ArrayList.

--- week3F-InheritanceSuperKeywordGeneralization/src/edu/mu/banking/client/Client.java
+++ week3F-InheritanceSuperKeywordGeneralization/src/edu/mu/banking/client/Client.java
@@ -1,11 +1,13 @@
 package edu.mu.banking.client;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import edu.mu.banking.account.Account;
+import edu.mu.banking.account.SavingsAccount;
 
 public class Client {
-	
+
 	private String name;
 	/*
 	 *  Create an Account type ArrayList member.
@@ -15,10 +17,60 @@ public class Client {
 	 *  to have a SavingsAccount or a CheckingAccount.
 	 */
 	private ArrayList<Account> accounts;
-	
+
 	public Client(String name) {
 		this.name = name;
 		this.accounts = new ArrayList<Account>();
+	}
+
+	public boolean closeAllSavingsAccount() {
+		boolean flag = false;
+//		for(int i=0; i<accounts.size(); i++) {
+//			if(accounts.get(i) instanceof SavingsAccount) {
+//				accounts.remove(i);
+//				flag = true;
+//				i--;
+//			}
+//		}
+		
+		Iterator<Account> it = accounts.iterator();
+		while(it.hasNext()) {
+			Account acc = it.next();
+			if(acc instanceof SavingsAccount) {
+				it.remove();
+				flag = true;
+			}
+		}
+		return flag;
+	}
+
+	public boolean closeAllAccount() {
+		boolean flag = false;
+		/*
+		 * This could result in ConcurrentModificationException
+		 * or IndexOutOfBoundsException, and is not safe. Use an
+		 * Iterator for to avoid such exceptions.
+		 */
+//		for(int i=0; i<accounts.size(); i++) {
+//			accounts.remove(i);
+//			flag = true;
+//		}
+//		return flag;
+		/*
+		 * Use an iterator while removing elements from the ArrayList
+		 */
+		Iterator<Account> it = accounts.iterator();
+		while(it.hasNext()) {
+			it.next();
+			it.remove();
+		}
+		return flag;
+	}
+
+	public void listOpenAccounts() {
+		for(Account acc : accounts) {
+			System.out.println("Account: " + acc);
+		}
 	}
 
 	public String getName() {
@@ -36,7 +88,4 @@ public class Client {
 	public void setAccounts(ArrayList<Account> accounts) {
 		this.accounts = accounts;
 	}
-	
-	
-
 }
