e68f3aac2538016a1d2a11a325b7bf543ff8847e
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-04-12 09:40:13 -0500

    An example the violates the 'I' principle

---
+++ SOLIDExamples/src/edu/mu/solid/isp/bad/Robot.java
@@ -0,0 +1,11 @@
+package edu.mu.solid.isp.bad;
+
+public class Robot implements Worker {
+	public void work() {
+		// robot working
+	}
+
+	public void eat() {
+		// robot eating (unnecessary for a robot)
+	}
+}
---
+++ SOLIDExamples/src/edu/mu/solid/isp/bad/Worker.java
@@ -0,0 +1,10 @@
+package edu.mu.solid.isp.bad;
+
+//Bad example: Fat interface
+public interface Worker {
+	void work();
+	void eat();
+}
+
+
+
